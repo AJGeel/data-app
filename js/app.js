8506012eab7f3bb59c2429d36702ebf7a3299575
AJGeel <geelaj01@gmail.com>
2019-05-14 14:21:42 +0200

    initial commit

---
+++ js/app.js
@@ -0,0 +1,101 @@
+var p1_presence = false;
+var p2_presence = false;
+var p3_presence = false;
+var guests_present = 0;
+
+var amountOfDataLinesSent = 0;
+
+const guests_present_container = document.getElementById('person_guests');
+const guests_currently_present = document.getElementById('number_present');
+
+const add_guest_remove = document.getElementById('btn_remove');
+add_guest_remove.addEventListener("click", function(){
+    if( guests_present > 0){
+      guests_present -= 1;
+    } else {
+      console.log("Can't remove any more guests.");
+    }
+
+    updateGuestValue();
+})
+
+const add_guest_btn = document.getElementById('btn_add');
+add_guest_btn.addEventListener("click", function(){
+  if( guests_present < 9){
+    guests_present += 1;
+  } else {
+    console.log("Can't add any more guests.");
+  }
+
+  updateGuestValue();
+})
+
+function updateGuestValue(){
+  if (guests_present == 0){
+    guests_present_container.classList.remove('active');
+  } else {
+    // add the active class
+    guests_present_container.classList.add('active');
+  }
+  guests_currently_present.innerHTML = guests_present;
+}
+
+function logAllPresent(){
+  console.log(`Is Sark home? ${(p1_presence.toString()).toUpperCase()}`);
+  console.log(`Is Arthur home? ${(p2_presence.toString()).toUpperCase()}`);
+  console.log(`Is Yizhou home? ${(p3_presence.toString()).toUpperCase()}`);
+  console.log(`There are currently ${guests_present} guests present.`);
+}
+
+function clickPerson(elem) {
+
+  for (var i = 0; i < elem.length; i++) {
+
+    elem[i].addEventListener("click", function(e) {
+      var current = this;
+
+      // console.log("CLICKED");
+
+      if (this.classList.contains('active') === true){
+        this.classList.remove('active');
+
+        if (this.innerHTML === 'Sark'){
+          p1_presence = false;
+        } else if (this.innerHTML === 'Arthur'){
+          p2_presence = false;
+        } else if (this.innerHTML === 'Yizhou'){
+          p3_presence = false;
+        }
+
+      } else {
+        this.classList.add('active');
+
+        if (this.innerHTML === 'Sark'){
+          p1_presence = true;
+        } else if (this.innerHTML === 'Arthur'){
+          p2_presence = true;
+        } else if (this.innerHTML === 'Yizhou'){
+          p3_presence = true;
+        }
+      }
+
+      e.preventDefault();
+    });
+  };
+}
+
+function sendPresenceData(){
+  // logAllPresent();
+  // console.log(' ');
+  let now = new Date();
+
+  console.log(`Data has been sent. Entry number ${amountOfDataLinesSent} at ${now}`);
+
+}
+
+var interval = setInterval(function() {
+  sendPresenceData();
+  amountOfDataLinesSent+=1;
+}, 5000)
+
+clickPerson(document.querySelectorAll('.person'));
