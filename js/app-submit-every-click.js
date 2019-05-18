cc221bdd88117a3c3562f366ff0226055484b0e9
AJGeel <geelaj01@gmail.com>
2019-05-18 10:04:21 +0200

    App now submits data every 30 seconds, regardless of interactions. Previous version can be found in 'app-submit-every-click.js'

--- js/app-submit-every-click.js
+++ js/app-submit-every-click.js
@@ -1,39 +1,26 @@
-/* 'Air Quality in Shared Environments', a web application developed by Arthur Geel.
-   In collaboration with Sark Xing and Yizhou Liu, for 'DBM160 — Data-Enabled Design', a course at
-   the Eindhoven University of Technology, faculty Industrial Design.
-
-   This web app serves as a data-gathering prototype: collecting data on the kitchen located in
-   'Huize Hertog'. All 'Huize Hertog' residents are asked to report whenever they enter or leave the shared kitchen.
-
-   In future releases, this application will support self-evaluations of the kitchen, and send insights to the residents.
-*/
-
-
-// Initializing global variables for the things we want to measure: 'Huize Hertogs' presence over time.
-// In this case, we have four main research participants (listed from p1 through p4), and we also account
-// for a total of 9 guests.
 var p1_presence = false;
 var p2_presence = false;
 var p3_presence = false;
 var p4_presence = false;
 var guests_present = 0;
 
-// Initialize a global variable for the parameter string that fill be fed to the PHP script, server-side.
 var paramsToPHP = "placeholder";
 
-// Selecting the DOM elements for the persons- and guests present.
+
 const guests_present_container = document.getElementById('person_guests');
 const guests_currently_present = document.getElementById('number_present');
 
 const add_guest_remove = document.getElementById('btn_remove');
 add_guest_remove.addEventListener("click", function(){
+
     if( guests_present > 0){
       guests_present -= 1;
     } else {
       console.log("Can't remove any more guests.");
     }
+
     updateGuestValue();
-    // sendDataToPHP();       // Uncomment this if you would like to submit data every user interaction.
+    sendDataToPHP();
 })
 
 const add_guest_btn = document.getElementById('btn_add');
@@ -46,23 +33,21 @@ add_guest_btn.addEventListener("click", function(){
   }
 
   updateGuestValue();
-  // sendDataToPHP();       // Uncomment this if you would like to submit data every user interaction.
+  sendDataToPHP();
 })
 
 function updateGuestValue(){
   if (guests_present == 0){
-    // If there are no guests present, remove the 'active' class from the DOM element.
     guests_present_container.classList.remove('active');
   } else {
-    // If there are >= 1 guests present, add the 'active' class to the DOM element.
+    // add the active class
     guests_present_container.classList.add('active');
   }
-  // Update the innerHTML value of the amount of guests present.
   guests_currently_present.innerHTML = guests_present;
 }
 
-
 function clickPerson(elem) {
+
   for (var i = 0; i < elem.length; i++) {
 
     elem[i].addEventListener("click", function(e) {
@@ -94,17 +79,17 @@ function clickPerson(elem) {
           p4_presence = true;
         }
       }
-      // sendDataToPHP();       // Uncomment this if you would like to submit data every user interaction.
+
+      sendDataToPHP();
+
       e.preventDefault();
     });
   };
 
 }
 
-// Adds the eventlistener to all persons in the HTML page
 clickPerson(document.querySelectorAll('.person'));
 
-// Function that generates a valid parameter string that is to be fed in the PHP script.
 function generateParamsForPHP(){
   paramsToPHP = "p1=" + p1_presence + "&p2=" + p2_presence + "&p3=" + p3_presence + "&p4=" + p4_presence + "&num_guests=" + guests_present;
 }
@@ -133,10 +118,4 @@ function sendDataToPHP(){
   makeXHR();
 }
 
-// Function that submits a data entry every 30 seconds
-setInterval(function(){
-  sendDataToPHP();
-}, 30000);
-
-// Upon loading the full page, submit a data entry
 sendDataToPHP();
